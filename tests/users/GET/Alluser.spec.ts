311bf644e9c99b19bec492cf952137310b24e8f4
Fikriaz <fikri.achmadfauzi77@gmail.com>
2026-07-25 21:35:27 +0700

    add all user

---
+++ tests/users/GET/Alluser.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from "@playwright/test";
+import Ajv from "ajv";
+import { categoriesSchema } from "../../schemas/categoriesSchema";
+import * as fs from "fs";
+const ajv = new Ajv();
+const apiUrl = process.env.apiUrl as string;
+const { accessToken } = JSON.parse(fs.readFileSync("tokens.json", "utf-8"));
+
+test.describe("GET/ Categories Product", () => {
+
+  test("GET / All User", async ( {request }) => {
+    const response = await request.get(apiUrl + '/users',
+    {
+    headers: {
+    Authorization: `Bearer ${accessToken}`,
+    }
+    });
+    
+    const body = await response.json();
+    expect(response.status()).toBe(200);
+    console.log(body);
+    expect(typeof body.users.firstName).toBe('string')
+    expect(typeof body.users.middleName).toBe('string')
+    expect(typeof body.users.lastName).toBe('string')
+    expect(typeof body.users.email).toBe('string')
+    expect(body.users.email).toMatch(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+
+    });
+
+test("GET / All Products By Categories", async ( {request }) => {
+    const response = await request.get(apiUrl + '/products/categories',
+    {
+    headers: {
+    Authorization: `Bearer ${accessToken}`,
+    },
+    data: {
+    limit: 5,
+    skip: 10,
+    select: "firstName,age" 
+    }
+    });
+
+    const body = await response.json();
+    expect(response.status()).toBe(200);
+    console.log(body);
+    expect(body.users.id).toBeDefined();
+    expect(body.users.firstName).toBeDefined();
+    expect(body.users.age).toBeDefined();
+    
+    expect(body.skip).toBe(10);
+    expect(body.limit).toBe(5);
+   
+    });    
+  });
+
