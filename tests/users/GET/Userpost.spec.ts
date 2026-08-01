b6b4aaca2bcc5f3f3cbdc3c0390848b0e3affdbb
Fikriaz <fikri.achmadfauzi77@gmail.com>
2026-08-02 01:05:49 +0700

    add user post

---
+++ tests/users/GET/Userpost.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+import Ajv from "ajv";
+import { userSchema } from "../../schemas/userSchema";
+import * as fs from "fs";
+const ajv = new Ajv();
+const apiUrl = process.env.apiUrl as string;
+const { accessToken } = JSON.parse(fs.readFileSync("tokens.json", "utf-8"));
+
+test.describe("GET/ User post", () => {
+
+  test("GET / user post", async ( {request }) => {
+    const response = await request.get(apiUrl + '/users/1/posts',
+    {
+    headers: {
+    Authorization: `Bearer ${accessToken}`,
+    }
+    });
+    const body = await response.json();
+    expect(response.status()).toBe(200);
+
+    expect(body.posts).toBeDefined();
+    expect(body.posts).not.toBeNull();
+    expect(body.posts.userid).toBe(1);
+
+  });
+
+});
+
